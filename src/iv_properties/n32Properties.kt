98009515c9976ebbe35d56a824baf176aec4ffa4
stf <dev@st-f.net>
2018-07-19 15:23:18 +0100

    added build folder to make test results accessible

--- src/iv_properties/n32Properties.kt
+++ src/iv_properties/n32Properties.kt
@@ -3,17 +3,21 @@ package iv_properties
 import util.TODO
 import util.doc32
 
-class PropertyExample() {
+class PropertyExample {
     var counter = 0
-    var propertyWithCounter: Int? = todoTask32()
+    var propertyWithCounter: Int? = null
+        set(value) {
+            counter++
+            field = value
+        }
 }
 
 fun todoTask32(): Nothing = TODO(
-    """
+        """
         Task 32.
         Add a custom setter to 'PropertyExample.propertyWithCounter' so that
         the 'counter' property is incremented every time 'propertyWithCounter' is assigned to.
     """,
-    documentation = doc32(),
-    references = { PropertyExample() }
+        documentation = doc32(),
+        references = { PropertyExample() }
 )
